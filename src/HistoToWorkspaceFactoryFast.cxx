44d3c1e7f40714af159e1214315bd84d0c340513
George Lewis <ghl227@nyu.edu>
2012-09-12 16:16:46 -0400

    When creating a NormFactory, only create a new variable if the NormFactor isn't already in the Workspace (could be an expression)

--- src/HistoToWorkspaceFactoryFast.cxx
+++ src/HistoToWorkspaceFactoryFast.cxx
@@ -506,7 +506,14 @@ namespace HistFactory{
         else {
           varname=itr->name;
         }
-        proto->factory((varname+range.str()).c_str());
+
+	// GHL: Check that the NormFactor doesn't already exist
+	//      (it may have been created as a function expression
+	//       during preprocessing)
+	if( proto->obj(varname.c_str()) == NULL) {
+	  proto->factory((varname+range.str()).c_str());
+	}
+
 	if(itr->constant){
 	  //	  proto->var(varname.c_str())->setConstant();
 	  //	  cout <<"setting " << varname << " constant"<<endl;
